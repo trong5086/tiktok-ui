9c5b923d319e377da7caa3f965609d8357caa08a
trong5086 <19522411@gm.uit.edu.vn>
2023-08-08 15:42:20 +0700

    Xây dựng UI phần header

---
+++ src/assets/images/index.js
@@ -0,0 +1,6 @@
+const images = {
+    logo: require('~/assets/images/logo.svg').default,
+    clear: require('~/assets/images/clear.svg').default,
+    search: require('~/assets/images/search.svg').default,
+};
+export default images;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,10 +1,27 @@
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+
 import styles from './Header.module.scss';
+import images from '~/assets/images';
 const cx = classNames.bind(styles);
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <img src={images.logo} alt="Tiktok" />
+                <div className={cx('search')}>
+                    <input placeholder="Tìm kiếm" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <img src={images.clear} alt="clear" />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <img src={images.search} alt="clear" />
+                    </button>
+                </div>
+                <div className={cx('actions')}></div>
+            </div>
         </header>
     );
 }
