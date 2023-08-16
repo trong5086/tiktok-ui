fed4a8af298ce5d5a83ba3f8d3512abcf08d59c6
trong5086 <19522411@gm.uit.edu.vn>
2023-08-16 17:26:03 +0700

    fix some bug trong css của GlobalStyle + sửa css Search + tách hàm cho rõ nghĩa

--- src/components/Popper/Menu/Menu.js
+++ src/components/Popper/Menu/Menu.js
@@ -35,6 +35,26 @@ function Menu({ children, items = [], hideOnClick = false, onChange = defaultFn 
         });
     };
 
+    const handleBack = () => {
+        setHistory((prev) => prev.slice(0, prev.length - 1));
+    };
+
+    const renderResults = (attrs) => {
+        return (
+            <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
+                <PopperWrapper className={cx('menu-popper')}>
+                    {history.length > 1 && <Header title={current.title} onBack={handleBack} />}
+                    <div className={cx('menu-body')}>{renderItems()}</div>
+                </PopperWrapper>
+            </div>
+        );
+    };
+
+    // Reset to first page
+    const handleReset = () => {
+        setHistory((prev) => prev.slice(0, 1));
+    };
+
     return (
         <Tippy
             interactive
@@ -42,24 +62,8 @@ function Menu({ children, items = [], hideOnClick = false, onChange = defaultFn 
             offset={[10, 10]}
             placement="bottom-end"
             hideOnClick={hideOnClick}
-            render={(attrs) => {
-                return (
-                    <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
-                        <PopperWrapper className={cx('menu-popper')}>
-                            {history.length > 1 && (
-                                <Header
-                                    title={current.title}
-                                    onBack={() => {
-                                        setHistory((prev) => prev.slice(0, prev.length - 1));
-                                    }}
-                                />
-                            )}
-                            <div className={cx('menu-body')}>{renderItems()}</div>
-                        </PopperWrapper>
-                    </div>
-                );
-            }}
-            onHide={() => setHistory((prev) => prev.slice(0, 1))}
+            render={renderResults}
+            onHide={handleReset}
         >
             {children}
         </Tippy>
--- src/layouts/components/Search/Search.js
+++ src/layouts/components/Search/Search.js
@@ -17,15 +17,15 @@ const cx = classNames.bind(styles);
 function Search() {
     const [searchValue, setSearchValue] = useState('');
     const [searchResult, setSearchResult] = useState([]);
-    const [showResult, setShowResult] = useState(true);
+    const [showResult, setShowResult] = useState(false);
     const [loading, setLoading] = useState(false);
 
-    const debounced = useDebounce(searchValue, 500);
+    const debouncedValue = useDebounce(searchValue, 500);
 
     const inputRef = useRef();
 
     useEffect(() => {
-        if (!debounced.trim()) {
+        if (!debouncedValue.trim()) {
             setSearchResult([]);
             return;
         }
@@ -33,7 +33,7 @@ function Search() {
         const fetchApi = async () => {
             setLoading(true);
 
-            const result = await searchService.search(debounced);
+            const result = await searchService.search(debouncedValue);
 
             setSearchResult(result);
 
@@ -41,7 +41,7 @@ function Search() {
         };
 
         fetchApi();
-    }, [debounced]);
+    }, [debouncedValue]);
 
     const handleChange = (e) => {
         const searchValue = e.target.value;
