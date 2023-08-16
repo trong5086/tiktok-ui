fb789392d121bf622c7a5aa2f9813a3cc294328e
trong5086 <19522411@gm.uit.edu.vn>
2023-08-16 16:45:52 +0700

    Giải quyết vấn đề file index lặp lại

---
+++ src/layouts/MainLayout/MainLayout.js
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
+import Header from '~/layouts/components/Header';
+import styles from './MainLayout.module.scss';
+import Sidebar from './Sidebar';
+const cx = classNames.bind(styles);
+function MainLayout({ children }) {
+    return (
+        <div className={cx('wrapper')}>
+            <Header />
+            <div className={cx('container')}>
+                <Sidebar />
+                <div className={cx('content')}>{children}</div>
+            </div>
+        </div>
+    );
+}
+
+MainLayout.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+export default MainLayout;
